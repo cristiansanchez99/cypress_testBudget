4c0f2ca41555ec1c53a39ce2b0f5ab7effbc8ff5
Cristian Sanchez A <49497909+cristiansanchez99@users.noreply.github.com>
2023-02-22 14:41:23 -0500

    Update actions.spec.js

--- cypress/integration/examples/actions.spec.js
+++ cypress/integration/examples/actions.spec.js
@@ -2,10 +2,10 @@
 
 context('Actions', () => {
   beforeEach(() => {
-    cy.visit('https://example.cypress.io/commands/actions')
+  
   })
 
-  // https://on.cypress.io/interacting-with-elements
+
 
   it('.type() - type into a DOM element', () => {
     // https://on.cypress.io/type
