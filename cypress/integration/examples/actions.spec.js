1a6b6c7af60f3e1dc5f7eb4c9911553db56a9444
Cristian Sanchez A <49497909+cristiansanchez99@users.noreply.github.com>
2023-02-22 15:13:21 -0500

    Update actions.spec.js

--- cypress/integration/examples/actions.spec.js
+++ cypress/integration/examples/actions.spec.js
@@ -1,16 +1,16 @@
-/// <reference types="cypress" />
+// <reference types="cypress" />
 
 context('Actions', () => {
   beforeEach(() => {
-  
+    cy.visit('https://example.cypress.io/commands/actions')
   })
 
-
+  // https://on.cypress.io/interacting-with-elements
 
   it('.type() - type into a DOM element', () => {
     // https://on.cypress.io/type
     cy.get('.action-email')
-      .type('fakeemail.com').should('have.value', 'fakeemail.com')
+      .type('fake@email.com').should('have.value', 'fake@email.com')
 
       // .type() with special character sequences
       .type('{leftarrow}{rightarrow}{uparrow}{downarrow}')
